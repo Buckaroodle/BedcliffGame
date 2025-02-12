dcec65aed5712c5ed6fbc9532837fb05fa0e2a49
Buckaroodle <aaron610@icloud.com>
2025-02-11 23:44:41 -0500

    WORKING CHARACTER

--- bedcliff/rigid_body_2d_2.gd
+++ bedcliff/rigid_body_2d_2.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@export var bed : Node2D 
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -11,8 +12,13 @@ func _physics_process(delta):
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump2") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	move_and_slide()
+		
+func _process(delta):
+	global_position.x = bed.global_position.x
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -22,4 +28,4 @@ func _physics_process(delta):
 	# else:
 	# 	velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+	
