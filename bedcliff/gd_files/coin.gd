ec967612f80b3c2f011d486ae08338c8586f2072
Buckaroodle <aaron610@icloud.com>
2025-02-24 22:14:24 -0500

    coin sfx works!

--- bedcliff/gd_files/coin.gd
+++ bedcliff/gd_files/coin.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var coinSound = $CoinSound
+var coinSoundEffect = preload("res://scenes/coinsoundeffect.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,7 +13,9 @@ func coinGet():
 func _on_body_entered(body):
 	if body.name == "BottomCliff" or "TopBed":
 		$Coin.visible = not $Coin.visible
-		coinSound.play()
+		var playsfx = coinSoundEffect.instantiate()
+		var parent = self.get_parent()
+		parent.add_child(playsfx)
 		print ("hi")
-		await get_tree().create_timer(.78).timeout
+		# await get_tree().create_timer(.78).timeout
 		queue_free()
